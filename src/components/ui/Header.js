d09c480d299b6c274fac2e5e9b0dff6e3f781596
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-14 21:06:11 +0900

    【セクション2】13. The AppBar Component
    On branch master
    Changes to be committed:
    	deleted:    src/App.js
    	new file:   src/components/App.js
    	new file:   src/components/ui/Header.js
    	modified:   src/index.js

---
+++ src/components/ui/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+export default function Header(props) {
+  return (
+    <AppBar position='static'>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  );
+}
