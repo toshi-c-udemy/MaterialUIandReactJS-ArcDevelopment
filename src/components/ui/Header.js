c939df53d61a602945e3948cd5b81dac8986fc5c
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-15 06:30:04 +0900

    【セクション2】29. Active Tab Refresh Fix
    On branch master
    Changes to be committed:
    	modified:   src/components/ui/Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppBar, Toolbar, Tabs, Tab, Button } from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
@@ -24,7 +24,11 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
     marginBottom: '3em',
   },
-  logo: { height: '7em' },
+  logo: { height: '8em' },
+  logoContainer: {
+    padding: 0,
+    '&:hover': { backgroundColor: 'transparent' },
+  },
   tabContainer: {
     marginLeft: 'auto',
   },
@@ -51,12 +55,36 @@ export default function Header(props) {
     setValue(value);
   };
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
           <Toolbar disableGutters>
-            <img alt='company logo' className={classes.logo} src={logo} />
+            <Button
+              component={Link}
+              to='/'
+              disableRipple
+              onClick={() => setValue(0)}
+              className={classes.logoContainer}
+            >
+              <img alt='company logo' className={classes.logo} src={logo} />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
