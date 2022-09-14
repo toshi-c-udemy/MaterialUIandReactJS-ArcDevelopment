747fabe61676c123b464acf67095d2fcaf29a169
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-15 02:45:06 +0900

    【セクション2】24. Using Tabs
    On branch master
    Changes to be committed:
    	modified:   public/index.html
    	modified:   src/components/ui/Header.js
    	modified:   src/components/ui/Theme.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, Tabs, Tab } from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
 
@@ -24,6 +24,14 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em',
   },
   logo: { height: '7em' },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
 }));
 
 export default function Header(props) {
@@ -34,6 +42,13 @@ export default function Header(props) {
         <AppBar position='fixed' color='primary'>
           <Toolbar disableGutters>
             <img alt='company logo' className={classes.logo} src={logo} />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label='Home' />
+              <Tab className={classes.tab} label='Service' />
+              <Tab className={classes.tab} label='The Revolution' />
+              <Tab className={classes.tab} label='About Us' />
+              <Tab className={classes.tab} label='Contact Us' />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
