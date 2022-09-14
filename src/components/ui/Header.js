6248984527e462465032b9afdb0b59753c5a3ec1
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-15 05:06:01 +0900

    【セクション2】26. Using Buttons
    On branch master
    Changes to be committed:
    	modified:   src/components/ui/Header.js
    	modified:   src/components/ui/Theme.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Tabs, Tab } from '@material-ui/core';
+import { AppBar, Toolbar, Tabs, Tab, Button } from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
 
@@ -32,6 +32,14 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: '25px',
   },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+
+    height: '45px',
+  },
 }));
 
 export default function Header(props) {
@@ -49,6 +57,9 @@ export default function Header(props) {
               <Tab className={classes.tab} label='About Us' />
               <Tab className={classes.tab} label='Contact Us' />
             </Tabs>
+            <Button variant='contained' color='secondary' className={classes.button}>
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
