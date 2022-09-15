6cfc061974d373d154f1dcb30ce6d939457184f3
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-16 06:52:14 +0900

    【セクション3】49. Section Cleanup Continued
    On branch MaterialUIandReactJS/Section3-MenusAndResponsiveDesign
    Changes to be committed:
    	modified:   src/components/ui/Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -163,7 +163,14 @@ export default function Header(props) {
 
   const routes = [
     { name: 'Home', link: '/', activeIndex: 0 },
-    { name: 'Services', link: '/services', activeIndex: 1 },
+    {
+      name: 'Services',
+      link: '/services',
+      activeIndex: 1,
+      ariaOwns: anchorEl ? 'simple-menu' : undefined,
+      ariaPopup: anchorEl ? 'true' : undefined,
+      mouseOver: (event) => handleClick(event),
+    },
     { name: 'The Revolution', link: '/revolution', activeIndex: 2 },
     { name: 'About Us', link: '/about', activeIndex: 3 },
     { name: 'Contact Us', link: '/contact', activeIndex: 4 },
@@ -194,19 +201,18 @@ export default function Header(props) {
         className={classes.tabContainer}
         indicatorColor='primary'
       >
-        <Tab className={classes.tab} component={Link} to='/' label='Home' />
-        <Tab
-          aria-owns={anchorEl ? 'simple-menu' : undefined}
-          aria-haspopup={anchorEl ? 'true' : undefined}
-          className={classes.tab}
-          component={Link}
-          onClick={(event) => handleClick(event)}
-          to='/services'
-          label='Service'
-        />
-        <Tab className={classes.tab} component={Link} to='/revolution' label='The Revolution' />
-        <Tab className={classes.tab} component={Link} to='/about' label='About Us' />
-        <Tab className={classes.tab} component={Link} to='/contact' label='Contact Us' />
+        {routes.map((route, index) => (
+          <Tab
+            key={`${route}${index}`}
+            className={classes.tab}
+            component={Link}
+            to={route.link}
+            label={route.name}
+            aria-owns={route.ariaOwns}
+            aria-haspopup={route.ariaPopup}
+            onMouseOver={route.mouseOver}
+          ></Tab>
+        ))}
       </Tabs>
       <Button variant='contained' color='secondary' className={classes.button}>
         Free Estimate
@@ -219,6 +225,7 @@ export default function Header(props) {
         classes={{ paper: classes.menu }}
         MenuListProps={{ onMouseLeave: handleClose }}
         elevation={0}
+        keepMounted
       >
         {menuOptions.map((option, i) => (
           <MenuItem
@@ -251,106 +258,32 @@ export default function Header(props) {
         classes={{ paper: classes.drawer }}
       >
         <List disablePadding>
-          <ListItem
-            onClick={() => {
-              setOpenDrawer(false);
-              setValue(0);
-            }}
-            divider
-            button
-            component={Link}
-            to='/'
-            selected={value === 0}
-          >
-            <ListItemText
-              className={
-                value === 0 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem
-              }
-              disableTypography
+          {routes.map((route, index) => (
+            <ListItem
+              divider
+              key={`${route}${route.activeIndex}`}
+              button
+              component={Link}
+              to={route.link}
+              selected={value === route.activeIndex}
+              onClick={() => {
+                setOpenDrawer(false);
+                setValue(route.activeIndex);
+              }}
             >
-              Home
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {
-              setOpenDrawer(false);
-              setValue(1);
-            }}
-            divider
-            button
-            component={Link}
-            to='/services'
-            selected={value === 1}
-          >
-            <ListItemText
-              className={
-                value === 1 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem
-              }
-              disableTypography
-            >
-              Services
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {
-              setOpenDrawer(false);
-              setValue(2);
-            }}
-            divider
-            button
-            component={Link}
-            to='/revolution'
-            selected={value === 2}
-          >
-            <ListItemText
-              className={
-                value === 2 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem
-              }
-              disableTypography
-            >
-              The Revolution
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {
-              setOpenDrawer(false);
-              setValue(3);
-            }}
-            divider
-            button
-            component={Link}
-            to='/about'
-            selected={value === 3}
-          >
-            <ListItemText
-              className={
-                value === 3 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem
-              }
-              disableTypography
-            >
-              About Us
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {
-              setOpenDrawer(false);
-              setValue(4);
-            }}
-            divider
-            button
-            component={Link}
-            to='/contact'
-            selected={value === 4}
-          >
-            <ListItemText
-              className={
-                value === 4 ? [classes.drawerItem, classes.drawerItemSelected] : classes.drawerItem
-              }
-              disableTypography
-            >
-              Contact Us
-            </ListItemText>
-          </ListItem>
+              <ListItemText
+                className={
+                  value === route.activeIndex
+                    ? [classes.drawerItem, classes.drawerItemSelected]
+                    : classes.drawerItem
+                }
+                disableTypography
+              >
+                {route.name}
+              </ListItemText>
+            </ListItem>
+          ))}
+
           <ListItem
             onClick={() => {
               setOpenDrawer(false);
