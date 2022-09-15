e52e7f5095d81f01fd7ec372a2f23e4974b84ec4
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-16 02:02:30 +0900

    【セクション3】41. Using Media Queries
    On branch MaterialUIandReactJS/Section3-MenusAndResponsiveDesign
    Changes to be committed:
    	modified:   src/components/ui/Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Toolbar, Tabs, Tab, Button, Menu, MenuItem } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Tabs,
+  Tab,
+  Button,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+} from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
+import { useTheme } from '@material-ui/core/styles';
 
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
@@ -23,8 +33,22 @@ const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: '3em',
+    [theme.breakpoints.down('md')]: {
+      marginBottom: '2em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '1.25em',
+    },
   },
-  logo: { height: '8em' },
+  logo: {
+    height: '8em',
+    [theme.breakpoints.down('md')]: {
+      height: '7em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '5.5em',
+    },
+  },
   logoContainer: {
     padding: 0,
     '&:hover': { backgroundColor: 'transparent' },
@@ -61,6 +85,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
@@ -170,6 +196,60 @@ export default function Header(props) {
     }
   }, [value]);
 
+  const tabs = (
+    <React.Fragment>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        className={classes.tabContainer}
+        indicatorColor='primary'
+      >
+        <Tab className={classes.tab} component={Link} to='/' label='Home' />
+        <Tab
+          aria-owns={anchorEl ? 'simple-menu' : undefined}
+          aria-haspopup={anchorEl ? 'true' : undefined}
+          className={classes.tab}
+          component={Link}
+          onClick={(event) => handleClick(event)}
+          to='/services'
+          label='Service'
+        />
+        <Tab className={classes.tab} component={Link} to='/revolution' label='The Revolution' />
+        <Tab className={classes.tab} component={Link} to='/about' label='About Us' />
+        <Tab className={classes.tab} component={Link} to='/contact' label='Contact Us' />
+      </Tabs>
+      <Button variant='contained' color='secondary' className={classes.button}>
+        Free Estimate
+      </Button>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        classes={{ paper: classes.menu }}
+        MenuListProps={{ onMouseLeave: handleClose }}
+        elevation={0}
+      >
+        {menuOptions.map((option, i) => (
+          <MenuItem
+            key={option}
+            component={Link}
+            to={option.link}
+            classes={{ root: classes.menuItem }}
+            onClick={(event) => {
+              handleMenuItemClick(event, i);
+              setValue(1);
+              handleClose();
+            }}
+            selected={i === selectedIndex && value === 1}
+          >
+            {option.name}
+          </MenuItem>
+        ))}
+      </Menu>
+    </React.Fragment>
+  );
+
   return (
     <React.Fragment>
       <ElevationScroll>
@@ -184,60 +264,7 @@ export default function Header(props) {
             >
               <img alt='company logo' className={classes.logo} src={logo} />
             </Button>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              className={classes.tabContainer}
-              indicatorColor='primary'
-            >
-              <Tab className={classes.tab} component={Link} to='/' label='Home' />
-              <Tab
-                aria-owns={anchorEl ? 'simple-menu' : undefined}
-                aria-haspopup={anchorEl ? 'true' : undefined}
-                className={classes.tab}
-                component={Link}
-                onClick={(event) => handleClick(event)}
-                to='/services'
-                label='Service'
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to='/revolution'
-                label='The Revolution'
-              />
-              <Tab className={classes.tab} component={Link} to='/about' label='About Us' />
-              <Tab className={classes.tab} component={Link} to='/contact' label='Contact Us' />
-            </Tabs>
-            <Button variant='contained' color='secondary' className={classes.button}>
-              Free Estimate
-            </Button>
-            <Menu
-              id='simple-menu'
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              classes={{ paper: classes.menu }}
-              MenuListProps={{ onMouseLeave: handleClose }}
-              elevation={0}
-            >
-              {menuOptions.map((option, i) => (
-                <MenuItem
-                  key={option}
-                  component={Link}
-                  to={option.link}
-                  classes={{ root: classes.menuItem }}
-                  onClick={(event) => {
-                    handleMenuItemClick(event, i);
-                    setValue(1);
-                    handleClose();
-                  }}
-                  selected={i === selectedIndex && value === 1}
-                >
-                  {option.name}
-                </MenuItem>
-              ))}
-            </Menu>
+            {matches ? null : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
