eca63c3efa31aaf9e42bfde47831c68dca68234c
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-17 23:54:02 +0900

    【セクション4】59. Social Media Icons
    On branch MaterialUIandReactJS/Section4-FootersAndBasicLayouts
    Changes to be committed:
    	modified:   src/components/ui/Footer.js

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,6 +4,9 @@ import { makeStyles } from '@material-ui/styles';
 import { Grid, Hidden } from '@material-ui/core';
 
 import footerAdornment from '../../assets/Footer Adornment.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -33,6 +36,22 @@ const useStyles = makeStyles((theme) => ({
   },
   gridItem: {
     margin: '3em',
+  },
+  icon: {
+    height: '4em',
+    width: '4em',
+    [theme.breakpoints.down('xs')]: {
+      height: '2.5em',
+      width: '2.5em',
+    },
+  },
+  socialContainer: {
+    position: 'absolute',
+    marginTop: '-6em',
+    right: '1.5em',
+    [theme.breakpoints.down('xs')]: {
+      right: '0.6em',
+    },
   },
 }));
 
@@ -204,6 +223,35 @@ export default function Footer(props) {
         </Grid>
       </Hidden>
       <img alt='black decorative slash' src={footerAdornment} className={classes.adornment}></img>
+      <Grid container justify='flex-end' spacing={2} className={classes.socialContainer}>
+        <Grid
+          item
+          component={'a'}
+          href='http://www.facebook.com'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <img alt='facebook logo' src={facebook} className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={'a'}
+          href='http://www.twitter.com'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <img alt='twitter logo' src={twitter} className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={'a'}
+          href='http://www.instagram.com'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <img alt='instagram logo' src={instagram} className={classes.icon} />
+        </Grid>
+      </Grid>
     </footer>
   );
 }
