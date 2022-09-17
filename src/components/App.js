fbb23d7d8977e6f50030bafbfb532e2ceeae8182
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-17 17:59:15 +0900

    【セクション4】57. Active Tab Footer Refactor
    On branch MaterialUIandReactJS/Section4-FootersAndBasicLayouts
    Changes to be committed:
    	modified:   src/components/App.js
    	modified:   src/components/ui/Footer.js
    	modified:   src/components/ui/Header.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
@@ -7,10 +7,18 @@ import Header from '../components/ui/Header';
 import Footer from '../components/ui/Footer';
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
+
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header />
+        <Header
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
         <Switch>
           <Route
             exact
@@ -26,7 +34,12 @@ function App() {
           <Route exact path='/contact' component={() => <div>Contact</div>}></Route>
           <Route exact path='/estimate' component={() => <div>Estimate</div>}></Route>
         </Switch>
-        <Footer />
+        <Footer
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
       </BrowserRouter>
     </ThemeProvider>
   );
