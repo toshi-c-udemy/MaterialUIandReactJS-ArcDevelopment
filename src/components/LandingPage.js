7ea6e17334b10cd22353d8c562ce7ebc36c2247f
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-21 17:16:46 +0900

    【セクション5】64. Creating The Hero Block
    On branch MaterialUIandReactJS/Section5-AnimationAndTheLandingPage
    Changes to be committed:
    	modified:   src/components/LandingPage.js
    	new file:   src/components/ui/ButtonArrow.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Grid, Button } from '@material-ui/core';
 
 import animationData from '../animations/landinganimation/data';
+import ButtonArrow from '../components/ui/ButtonArrow';
 
 const useStyle = makeStyles((theme) => ({}));
 
@@ -18,5 +19,33 @@ export default function LandingPage() {
     },
   };
 
-  return <Lottie options={defaultOptions} height={'100%'} width={'100%'} />;
+  return (
+    <Grid container direction='column'>
+      <Grid item>
+        <Grid container direction='row'>
+          <Grid item>
+            <div>
+              Bring West Coast Technology
+              <br />
+              to the Midwest
+            </div>
+            <Grid container>
+              <Grid item>
+                <Button variant='contained'>Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined'>
+                  Learn More
+                  <ButtonArrow width={15} height={15} fill='red' />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
