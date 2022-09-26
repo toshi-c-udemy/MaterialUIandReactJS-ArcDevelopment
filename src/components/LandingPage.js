33f20d0e8e47ac4759f50ca697a2ba39c9a19758
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-27 06:39:33 +0900

    【セクション5】74. Creating The Information Block
    On branch MaterialUIandReactJS/Section5-AnimationAndTheLandingPage
    Changes to be committed:
    	modified:   src/components/LandingPage.js
    	modified:   src/components/ui/Theme.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -17,6 +17,7 @@ import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppIcon from '../assets/mobileIcon.svg';
 import webSiteIcon from '../assets/websiteIcon.svg';
 import revolutionBackground from '../assets/repeatingBackground.svg';
+import infoBackground from '../assets/infoBackground.svg';
 
 const useStyle = makeStyles((theme) => ({
   animation: {
@@ -111,6 +112,14 @@ const useStyle = makeStyles((theme) => ({
       paddingRight: 0,
       width: '100%',
     },
+  },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%',
   },
 }));
 
@@ -258,6 +267,7 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
+        {/*-----The Revolution Block-----*/}
         <Grid
           container
           style={{ height: '100em', marginTop: '12em' }}
@@ -286,6 +296,30 @@ export default function LandingPage() {
             </CardContent>
           </Card>
           <div className={classes.revolutionBackground} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*-----Information Block-----*/}
+        <Grid container style={{ height: '80em' }} alignItems='center' direction='row'>
+          <Grid item style={{ position: 'absolute', marginLeft: '5em' }}>
+            <Grid container direction='column'>
+              <Typography variant='h2' style={{ color: 'white' }}>
+                About Us
+              </Typography>
+              <Typography variant='subtitle2'>Let's get personal.</Typography>
+              <Grid item>
+                <Button
+                  variant='outlined'
+                  style={{ color: 'white', borderColor: 'white' }}
+                  className={classes.learnButtonHero}
+                >
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow width={15} height={15} fill='white' />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <div className={classes.infoBackground} />
         </Grid>
       </Grid>
     </Grid>
