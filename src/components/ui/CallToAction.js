cea7c630adfa8b80809ba5b3f7ca7333f482f916
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-27 10:08:45 +0900

    【セクション5】77. Creating The Call To Action Continued
    On branch MaterialUIandReactJS/Section5-AnimationAndTheLandingPage
    Changes to be committed:
    	modified:   src/components/ui/CallToAction.js

--- src/components/ui/CallToAction.js
+++ src/components/ui/CallToAction.js
@@ -2,13 +2,38 @@ import React from 'react';
 import { Grid, Typography, makeStyles, Button, useTheme } from '@material-ui/core';
 
 import ButtonArrow from './ButtonArrow';
+import background from '../../assets/background.jpg';
+import mobileBackground from '../../assets/mobileBackground.jpg';
 
 const useStyle = makeStyles((theme) => ({
-  learnButtonHero: {
+  learnButton: {
     ...theme.typography.learnButton,
-    fontSize: '0.9rem',
-    height: 45,
-    width: 145,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
+  },
+  background: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '60em',
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      backgroundImage: `url(${mobileBackground})`,
+    },
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    background: theme.palette.common.orange,
+    fontSize: '1.5rem',
+    marginRight: '5em',
   },
 }));
 
@@ -17,8 +42,8 @@ export default function CallToAction() {
   const theme = useTheme();
 
   return (
-    <Grid container>
-      <Grid item>
+    <Grid container alignItems='center' justify='space-between' className={classes.background}>
+      <Grid item style={{ marginLeft: '5em' }}>
         <Grid container direction='column'>
           <Grid item>
             <Typography variant='h2'>
@@ -26,15 +51,22 @@ export default function CallToAction() {
               <br />
               Revolutionary Results.
             </Typography>
-            <Typography variant='subtitle2'>Take advantage of the 21st Century.</Typography>
+            <Typography variant='subtitle2' style={{ fontSize: '1.5rem' }}>
+              Take advantage of the 21st Century.
+            </Typography>
             <Grid container item>
-              <Button variant='outlined' className={classes.learnButtonHero}>
+              <Button variant='outlined' className={classes.learnButton}>
                 <span style={{ marginRight: 5 }}>Learn More</span>
                 <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
               </Button>
             </Grid>
           </Grid>
         </Grid>
+      </Grid>
+      <Grid item>
+        <Button variant='contained' className={classes.estimateButton}>
+          Free Estimate
+        </Button>
       </Grid>
     </Grid>
   );
