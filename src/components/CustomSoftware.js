7f7279d96439cda40cc4c6df17cbd1bb65b51b3e
toshi-c-udemy <toshi.c.1986+udemy@gmail.com>
2022-09-27 20:34:41 +0900

    【セクション6】91. Custom Software Development Page - Responsive Design Continued
    On branch MaterialUIandReactJS/Section6-ComplexLayouts
    Changes to be committed:
    	modified:   src/components/CustomSoftware.js

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -23,6 +23,8 @@ import scaleAnimation from '../animations/scaleAnimation/data.json';
 import automationAnimation from '../animations/automationAnimation/data.json';
 import uxAnimation from '../animations/uxAnimation/data';
 
+import CallToAction from './ui/CallToAction';
+
 const useStyles = makeStyles((theme) => ({
   heading: {
     maxWidth: '40em',
@@ -30,11 +32,14 @@ const useStyles = makeStyles((theme) => ({
   arrowContainer: {
     marginTop: '0.5em',
   },
-  mainContainer: {
+  rowContainer: {
     paddingLeft: '5em',
     paddingRight: '5em',
-    paddingTop: '2em',
-    paddingBottom: '10em',
+
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '1.5em',
+      paddingRight: '1.5em',
+    },
   },
   itemContainer: {
     maxWidth: '40em',
@@ -46,6 +51,7 @@ export default function CustomSoftware(props) {
   const theme = useTheme();
   const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
 
   const documentOptions = {
     loop: true,
@@ -84,8 +90,15 @@ export default function CustomSoftware(props) {
   };
 
   return (
-    <Grid container direction='column' className={classes.mainContainer}>
-      <Grid item container direction='row' justify={matchesMD ? 'center' : undefined}>
+    <Grid container direction='column' className={classes.rowContainer}>
+      <Grid
+        item
+        container
+        direction='row'
+        justify={matchesMD ? 'center' : undefined}
+        className={classes.rowContainer}
+        style={{ marginTop: matchesXS ? '1em' : '2em' }}
+      >
         <Hidden mdDown>
           <Grid
             item
@@ -148,6 +161,7 @@ export default function CustomSoftware(props) {
         direction='row'
         justify='center'
         style={{ marginTop: '15em', marginBottom: '20em' }}
+        className={classes.rowContainer}
       >
         <Grid
           item
@@ -200,32 +214,36 @@ export default function CustomSoftware(props) {
         alignItems={matchesMD ? 'center' : undefined}
         direction={matchesMD ? 'column' : 'row'}
         justify='space-between'
+        className={classes.rowContainer}
       >
-        <Grid item container className={classes.itemContainer} md>
-          <Grid
-            item
-            container
-            direction='column'
-            style={{ marginBottom: matchesMD ? '15em' : 0 }}
-            md
-          >
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          style={{ marginBottom: matchesMD ? '15em' : 0 }}
+          md
+        >
+          <Grid item container direction='column' md>
             <Grid item>
-              <Typography variant='h4'>Digital Documents & Data</Typography>
+              <Typography variant='h4' align={matchesSM ? 'center' : undefined}>
+                Digital Documents & Data
+              </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' paragraph>
+              <Typography variant='body1' paragraph align={matchesSM ? 'center' : undefined}>
                 Reduce Errors. Reduce Waste. Reduce Costs.
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' paragraph>
+              <Typography variant='body1' paragraph align={matchesSM ? 'center' : undefined}>
                 Billions are spent annually on the purchasing, printing, and distribution of paper.
                 On top of the massive environmental impact this has, it causes harm to your bottom
                 line as well.
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' paragraph>
+              <Typography variant='body1' paragraph align={matchesSM ? 'center' : undefined}>
                 By utilizing digital forms and documents you can remove these obsolete expenses,
                 accelerate your communication, and help the Earth.
               </Typography>
@@ -239,7 +257,13 @@ export default function CustomSoftware(props) {
             />
           </Grid>
         </Grid>
-        <Grid item container className={classes.itemContainer} md>
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          md
+        >
           <Grid item md>
             <Lottie
               options={scaleOptions}
@@ -249,12 +273,12 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item container direction='column' md>
             <Grid item>
-              <Typography variant='h4' align='right'>
+              <Typography variant='h4' align={matchesSM ? 'center' : 'right'}>
                 Scale
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' align='right' paragraph>
+              <Typography variant='body1' align={matchesSM ? 'center' : 'right'} paragraph>
                 Whether you’re a large brand, just getting started, or taking off right now, our
                 application architecture ensures pain-free growth and reliability.
               </Typography>
@@ -262,10 +286,21 @@ export default function CustomSoftware(props) {
           </Grid>
         </Grid>
       </Grid>
-      <Grid item container direction='row' style={{ marginTop: '20em', marginBottom: '20em' }}>
+      <Grid
+        item
+        container
+        direction='row'
+        style={{ marginTop: '20em', marginBottom: '20em' }}
+        className={classes.rowContainer}
+      >
         <Grid item container direction='column' alignItems='center'>
           <Grid item>
-            <img src={roots} alt='tree with roots extending out' height='450em' width='450em' />
+            <img
+              src={roots}
+              alt='tree with roots extending out'
+              height={matchesSM ? '300em' : '450em'}
+              width={matchesSM ? '300em' : '450em'}
+            />
           </Grid>
           <Grid item className={classes.itemContainer}>
             <Typography variant='h4' align='center' gutterBottom>
@@ -287,32 +322,36 @@ export default function CustomSoftware(props) {
         alignItems={matchesMD ? 'center' : undefined}
         direction={matchesMD ? 'column' : 'row'}
         justify='space-between'
-        style={{ marginBottom: '10em' }}
+        style={{ marginBottom: '20em' }}
+        className={classes.rowContainer}
       >
         <Grid
           item
           container
           className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
           style={{ marginBottom: matchesMD ? '15em' : 0 }}
           md
         >
           <Grid item container direction='column' md>
             <Grid item>
-              <Typography variant='h4'>Automation</Typography>
+              <Typography variant='h4' align={matchesSM ? 'center' : undefined}>
+                Automation
+              </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' paragraph>
+              <Typography variant='body1' paragraph align={matchesSM ? 'center' : undefined}>
                 Why waste time when you don’t have to?
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' paragraph>
+              <Typography variant='body1' paragraph align={matchesSM ? 'center' : undefined}>
                 We can help you identify processes with time or event based actions which can now
                 easily be automated.
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' paragraph>
+              <Typography variant='body1' paragraph align={matchesSM ? 'center' : undefined}>
                 Increasing efficiency increases profits, leaving you more time to focus on your
                 business, not busywork.
               </Typography>
@@ -326,7 +365,13 @@ export default function CustomSoftware(props) {
             />
           </Grid>
         </Grid>
-        <Grid item container className={classes.itemContainer} md>
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          md
+        >
           <Grid item md>
             <Lottie
               options={uxOptions}
@@ -336,18 +381,18 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item container direction='column' md>
             <Grid item>
-              <Typography variant='h4' align='right'>
+              <Typography variant='h4' align={matchesSM ? 'center' : 'right'}>
                 User Experience Design
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant='body1' align='right' paragraph>
+              <Typography variant='body1' align={matchesSM ? 'center' : 'right'} paragraph>
                 A good design that isn’t usable isn’t a good design.
               </Typography>
-              <Typography variant='body1' align='right' paragraph>
+              <Typography variant='body1' align={matchesSM ? 'center' : 'right'} paragraph>
                 So why are so many pieces of software complicated, confusing, and frustrating?
               </Typography>
-              <Typography variant='body1' align='right' paragraph>
+              <Typography variant='body1' align={matchesSM ? 'center' : 'right'} paragraph>
                 By prioritizing users and the real ways they interact with technology we’re able to
                 develop unique, personable experiences that solve problems rather than create new
                 ones.
@@ -355,6 +400,9 @@ export default function CustomSoftware(props) {
             </Grid>
           </Grid>
         </Grid>
+      </Grid>
+      <Grid item>
+        <CallToAction setValue={props.setValue} />
       </Grid>
     </Grid>
   );
